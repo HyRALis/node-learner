93676cfcb8a5ae357b773229dcbc142ad27d48f5
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2024-12-09 14:12:43 +0100

    WORKING-WITH-EXPRESS: Added static folder public and added css stylesheet to the html pages

--- app.ts
+++ app.ts
@@ -5,12 +5,15 @@ import bodyParser from 'body-parser';
 import adminRouter from './routes/admin';
 import shopRouter from './routes/shop';
 import pageNotFoundPage from './pages/404';
+import path from 'path';
+import roodDirPath from './utils/rootDirPath';
 
 const PORT = 3000;
 
 const expressApp = express();
 
 expressApp.use(bodyParser.urlencoded({ extended: false }));
+expressApp.use(express.static(path.join(roodDirPath(), 'public')));
 
 expressApp.use('/admin', adminRouter);
 
